10f4aa0f6bd0a4bb9a6e3fdb90bc8d60a668369b
Alex Avitia <alex.avitia77@gmail.com>
2021-05-11 16:26:00 -0500

    added methods for add, search, delete

--- src/ContactManager/Manager.java
+++ src/ContactManager/Manager.java
@@ -9,9 +9,11 @@ import java.nio.file.Paths;
 
 public class Manager {
 
+    private static final Input scn = new Input();
+
     // Show all contacts
-    // TODO: Add a new contact
-    // TODO: Search a contact by her name
+    // Add a new contact
+    // Search a contact by her name
     // TODO: Delete an existing contact
 
 
@@ -25,19 +27,83 @@ public class Manager {
 
     }
 
+    public static void addContact(Path filePath, String contactName) throws IOException {
+
+        Files.write(
+                filePath,
+                Arrays.asList(contactName),
+                StandardOpenOption.APPEND
+        );
+    }
+
+    public static String searchContact(Path filePath, String contactName) throws IOException {
+
+        String match = "Match not found.";
+
+        Scanner scan = new Scanner(new FileInputStream(String.valueOf(filePath)));
+        while (scan.hasNextLine()) {
+            String currentLine = scan.nextLine();
+            if (currentLine.contains(contactName)) {
+                match = currentLine;
+            }
+        }
+        return match;
+    }
+
+    public static boolean ghostContact(Path filePath, String contactName) throws IOException {
+
+        List<String> handsomeList = new ArrayList<>();
+
+        Scanner scan = new Scanner(new FileInputStream(String.valueOf(filePath)));
+        while (scan.hasNextLine()) {
+            String currentLine = scan.nextLine();
+            if (!currentLine.contains(contactName)) {
+                handsomeList.add(currentLine);
+            } else {
+                System.out.println("Deleting " + currentLine);
+            }
+        }
+        Path deWay = Files.write(Paths.get(String.valueOf(filePath)), handsomeList);
+        return Files.exists(deWay);
+    }
+
+
+    // MAIN METHOD
     public static void main(String[] args) throws IOException {
         
         Path filePathToContacts = Paths.get("./src/ContactManager/contacts.txt");
 
         System.out.println("filePathToContacts = " + filePathToContacts);
 
-        printContacts(filePathToContacts);
+        System.out.println("1. View contacts");
+        System.out.println("2. Add a new contact");
+        System.out.println("3. Search a contact by name");
+        System.out.println("4. Delete an existing contact");
+        System.out.println("5. Exit");
 
-//        Files.write(
-//                Paths.get("data", String.valueOf(filePathToContacts)),
-//                Collections.singletonList("eggs"), // list with one item
-//                StandardOpenOption.APPEND
-//        );
+        int numInput = scn.getInt("\nWhat would you like to do?");
+        String contact;
 
+        switch (numInput) {
+            case 1 -> {
+                System.out.println("\nPrinting contacts...\n");
+                printContacts(filePathToContacts);
+            }
+            case 2 -> {
+                contact = scn.getString("\nTo create a new contact, enter their name, followed by their number with no dashes: ");
+                addContact(filePathToContacts, contact);
+                System.out.println("\nContact added.\n");
+            }
+            case 3 -> {
+                contact = scn.getString("\nWho are you looking for? \n");
+                System.out.println(searchContact(filePathToContacts, contact));
+            }
+            case 4 -> {
+                contact = scn.getString("\nEnter the name of who to delete: \n");
+                System.out.println(ghostContact(filePathToContacts, contact));
+
+            }
+            case 5 -> System.out.println("\nExiting...");
+        }
     }
 }
