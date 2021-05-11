3422818bb8a28c6c19945552f4f60bf32932e1d2
Alex Avitia <alex.avitia77@gmail.com>
2021-05-11 12:32:14 -0500

    added method to print contacts

--- src/ContactManager/Manager.java
+++ src/ContactManager/Manager.java
@@ -1,8 +1,42 @@
 package ContactManager;
 
+import java.io.*;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Manager {
 
-    public static void main(String[] args) {
+    // Show all contacts
+    // TODO: Add a new contact
+    // TODO: Search a contact by her name
+    // TODO: Delete an existing contact
+
+    public static void printContacts(Path filePath) throws IOException {
+
+        System.out.println();
+        List<String> fileContents = Files.readAllLines(filePath);
+        for (int i = 0; i < fileContents.size(); i++) {
+            System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        
+        Path filePathToContacts = Paths.get("./src/ContactManager/contacts.txt");
+
+        System.out.println("filePathToContacts = " + filePathToContacts);
+
+        printContacts(filePathToContacts);
+
+//        Files.write(
+//                Paths.get("data", String.valueOf(filePathToContacts)),
+//                Collections.singletonList("eggs"), // list with one item
+//                StandardOpenOption.APPEND
+//        );
 
     }
 }
